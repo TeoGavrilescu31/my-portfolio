e64e0bf01faa55251cddb18225ac4f8dbb4025af
Teo Gavrilescu <tigavrilescu@gmail.com>
2024-06-13 21:03:51 +0300

    Header setup - initial

--- app/page.tsx
+++ app/page.tsx
@@ -1,97 +1,34 @@
+import Profile from '../assets/images/profile-no-background.png'
 import Image from 'next/image'
+import Silhoutte from '../assets/images/silhouette.svg'
+import ScrollDown from '../assets/images/scroll-down.svg'
 import './page.scss'
 
-const Home = () => {
+const Page = () => {
   return (
     <main className={'main'}>
-      <div className={'description'}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={'code'}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={'vercelLogo'}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      <div className="hero-text-container">
+        <h1 className="hero-text">
+          Hello,
+          <br /> my name is
+          <br />
+          <span className="hero-text__name">Teo</span>
+        </h1>
       </div>
-
-      <div className={'center'}>
+      <div className="image-container">
+        <Image src={Silhoutte} alt="Silhoutte" className={'profile'} priority />
+      </div>
+      <div className="scroll-container">
+        <p className="scroll-container__text">Scroll down</p>
         <Image
-          className={'logo'}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
+          src={ScrollDown}
+          alt="Scroll Down"
+          className="scroll-container__icon"
           priority
         />
-      </div>
-
-      <div className={'grid'}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={'card'}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={'card'}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={'card'}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={'card'}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   )
 }
 
-export default Home
+export default Page
